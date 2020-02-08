94996a9fd4862cb8e6b1df517c34ee4cf03aaf2e
Subodh <42881171+subudear@users.noreply.github.com>
2020-02-08 18:04:18 +1300

    Update VM.tf

--- VM.tf
+++ VM.tf
@@ -1,3 +1,9 @@
+provider "azurerm" {
+    subscription_id = "fa628409-59a2-43ed-afde-7b509d932bbd"
+    client_id       = "165b883b-870b-4b58-a7b5-7b2c3be0df8a"
+    client_secret   = "ARGG0KSv_Lo:xBArjyKzux]mlcb8o6]2"
+    tenant_id       = "32a21b55-20a3-4804-9d5c-d6c478e84c53"
+}
 
 data "azurerm_resource_group" "rg" {
   name = "${var.resource_group_name}"
@@ -75,4 +81,23 @@ resource "azurerm_virtual_machine" "myterraformvm" {
     }
 }
 
-
+resource "azurerm_virtual_machine_extension" "msextension" {
+  name                 = "${var.vmName}-linuxagent"
+  location             = "${var.location}"
+  resource_group_name  = "${var.resource_group_name}"
+  virtual_machine_name = azurerm_virtual_machine.myterraformvm.name
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+  depends_on            = [azurerm_virtual_machine.myterraformvm]
+  auto_upgrade_minor_version = true
+  settings = <<SETTINGS
+    {
+        "fileUris": ["https://vstsaget.blob.core.windows.net/winfile/Ubuntu-vstsbuildagent-install.sh"],
+		"commandToExecute": "bash Ubuntu-vstsbuildagent-install.sh '${var.VSTSAccount}' '${var.PersonalAccessToken}' '${var.poolName}' '${var.vmName}' '${var.vmAdminUserName}' '${var.dockerVersion}'"
+    }
+    SETTINGS
+  tags = {
+    environment = "Production"
+  }
+}
