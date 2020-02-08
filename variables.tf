dc411ce68091161e64bedb6e26b9385518605fd8
Subodh <42881171+subudear@users.noreply.github.com>
2020-02-08 18:04:46 +1300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,75 +1,75 @@
 variable "vmName" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Provide VM name"
   default = "qatodhsub123"
 }
 
 variable "location" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Provide location where the resource will be created"
   default = "Australia Southeast"
 }
 
 variable "resource_group_name" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
-  default = "Testgroup"
+  description = "Provdie the resource group name"
+  default = "Testpoc"
 }
 
 variable "existingVirtualNetworkName" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Provide existing VNet Name"
   default = "vnet01qaodhsub"
 }
 
 variable "existingSubnetName" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Provide existing Subnet Name"
   default = "vnet01-qaodhsub"
 }
 
 variable "vmSize" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Provide VM size required"
   default = "Standard_B2ms"
 }
 
 variable "poolName" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
-  default = "linux pool"
+  description = "Provide PoolName where the agent will be created"
+  default = "linuxpool"
 }
 
 variable "PersonalAccessToken" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
-  default = "y437kbc645yy7uhe674d4546789oid6lebz5bya"
+  description = "Provide PErsonal Access Token"
+  default = "u3ymo3be7tjgryewjbbvlzb35p4vldc4xtb4dlcue6er3qq55zqq"
 }
 
 variable "VSTSAccount" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
-  default = "tes"
-}
-
-variable "teamProject" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
-  default = "tes"
+  description = "Provide Name of VSTS account"
+  default = "ghamad"
 }
 
 variable "vmAdminUserName" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Provide the admin user to login into VM"
   default = "testadmin"
 }
 
 variable "vmAdminPassword" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
-  default = "abc@123"
+  description = "Provide password for the admin of VM"
+  default = "P@ssw0rd01"
 }
 
 variable "imagePublisher" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "image publisher"
   default = "Canonical"
 }
 
 variable "imageOffer" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Image offer"
   default = "UbuntuServer"
 }
 
 variable "ubuntuOSVersion" {
-  description = "The prefix used for all resources in this example. Needs to be a short (6 characters) alphanumeric string. Example: `myprefix`."
+  description = "Provide the  OS version"
   default = "16.04.0-LTS"
+}
+
+variable "dockerVersion" {
+  description = "Provide Dcoker version to be installed"
+  default = "19.03.5"
 }
 
